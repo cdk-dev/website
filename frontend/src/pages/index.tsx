06e8cf85f269e4713ffef731038e443ba0ddc0c6
Sebastian Korfmann <sebastian@korfmann.net>
2020-08-14 11:54:53 +0200

    Make it a bit easier on the eye on mobile

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -7,7 +7,7 @@ function Index({}: Props): ReactElement {
   return (
     <>
       <section
-        class="homescreen m-0 flex flex-col w-screen justify-center bg-gray-800 h-screen text-gray-100 "
+        class="homescreen m-0 px-4 flex flex-col w-screen justify-center bg-gray-800 h-screen text-gray-100 "
       >
         <nav>
           <ul class="flex justify-between text-xl py-8 px-8 md:px-48 ">
@@ -21,8 +21,8 @@ function Index({}: Props): ReactElement {
         </nav>
         <h1 class="text-6xl my-auto mx-auto  md:mx-48 ">
           We're just <a href="https://dev.to/skorfmann/cdk-dev-call-for-contributors-4c46">getting started</a>.<br />
-          <span class="text-teal-400"><a href="https://github.com/cdk-dev/base">You can contribute</a></span>
-          <span class="ml-4">and<span class="text-teal-400"><a class="ml-4" href="https://join.slack.com/t/cdk-dev/shared_invite/zt-gff3dtkw-MsEPa5Id1Aey8HQUDEck1Q">join our Slack</a></span></span>
+          <span class="text-teal-400"><a href="https://github.com/cdk-dev/base">You can contribute</a></span><br />
+          and<span class="text-teal-400"><a class="ml-4" href="https://join.slack.com/t/cdk-dev/shared_invite/zt-gff3dtkw-MsEPa5Id1Aey8HQUDEck1Q">join our Slack</a></span>
         </h1>
       </section>
     </>

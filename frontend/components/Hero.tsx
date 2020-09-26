8d334e84a7cd673bbf36250f1b4e4d047cf4dd6a
Sebastian Korfmann <sebastian@korfmann.net>
2020-09-26 21:40:00 +0200

    Don't break into new line on smaller screens

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -209,7 +209,6 @@ function Nav({ title }: NavProps): ReactElement {
             <div className="text-center">
               <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
                 Welcome to cdk
-                <br className="xl:hidden" />
                 <span className="text-indigo-600">.dev</span>
               </h2>
               <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">

10f1baedf0504d76f5deae4a00689beebed8632b
Edwin <edwin.radtke@oblcc.com>
2020-09-26 18:06:57 +0200

    Minor fixes

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -7,7 +7,7 @@ function Footer(): ReactElement {
       <div className="max-w-screen-xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
         <nav className="-mx-5 -my-2 flex flex-wrap justify-center">
           <div className="px-5 py-2">
-            <a href="#" className="text-base leading-6 text-gray-500 hover:text-gray-900">
+            <a href="/" className="text-base leading-6 text-gray-500 hover:text-gray-900">
               Home
         </a>
           </div>
@@ -22,13 +22,13 @@ function Footer(): ReactElement {
         </a>
           </div>
           <div className="px-5 py-2">
-            <a href="https://www.cdkday.com/" className="text-base leading-6 text-gray-500 hover:text-gray-900">
-              CDK Day
+            <a href="https://github.com/cdk-dev" className="text-base leading-6 text-gray-500 hover:text-gray-900">
+              Github
         </a>
           </div>
           <div className="px-5 py-2">
-            <a href="https://github.com/cdk-dev" className="text-base leading-6 text-gray-500 hover:text-gray-900">
-              Github
+            <a href="https://www.cdkday.com/" className="text-base leading-6 text-gray-500 hover:text-gray-900">
+              CDK Day
         </a>
           </div>
 
--- frontend/components/Nav.tsx
+++ frontend/components/Nav.tsx
@@ -21,7 +21,11 @@ function Nav({ title }: NavProps): ReactElement {
                 cdk.dev
         </div>
         <div className="hidden sm:ml-6 sm:flex">
-          <a href="/tools" className="inline-flex items-center px-1 pt-1 border-b-2 border-indigo-500 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out">
+
+          <a href="/" className="inline-flex items-center px-1 pt-1 border-b-2 border-indigo-500 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out">
+            Home
+          </a>
+          <a href="/tools" className="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
             Tools
           </a>
           <a href="/resources" className="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">

5b5d50e94965edf976e6649534cac6b0ff5916f8
Sebastian Korfmann <sebastian@korfmann.net>
2020-09-27 21:49:59 +0200

    Auto format

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,16 +1,16 @@
 import React, { ReactElement } from "react"
-import Footer from './Footer'
-import Head from 'next/head'
+import Footer from "./Footer"
+import Head from "next/head"
 
 export default ({ children }): ReactElement => (
   <>
     <Head>
       <title>cdk.dev</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="stylesheet" href="https://rsms.me/inter/inter.css"/>
+      <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     </Head>
     <div className="min-h-screen bg-white">
-      { children }
+      {children}
       <Footer />
     </div>
   </>
--- frontend/pages/tools.tsx
+++ frontend/pages/tools.tsx
@@ -3,7 +3,7 @@ import React, { ReactElement } from "react"
 import Nav from "../components/Nav"
 import Layout from "../components/Layout"
 
-interface Props { }
+interface Props {}
 
 interface ProjectProps {
   title: string
@@ -13,10 +13,7 @@ interface ProjectProps {
 
 function Project({ title, href, description }: ProjectProps): ReactElement {
   return (
-    
-    <div className="rounded-lg bg-gray-900 mb-4 p-4 flex-1 m-2 flex flex-col items-start justify-between">
-
-    </div>
+    <div className="rounded-lg bg-gray-900 mb-4 p-4 flex-1 m-2 flex flex-col items-start justify-between"></div>
   )
 }
 
@@ -39,17 +36,20 @@ const projects: ProjectProps[] = [
   },
   {
     title: "projen",
-    description: "Define and maintain complex project configuration through code",
+    description:
+      "Define and maintain complex project configuration through code",
     href: `https://github.com/eladb/projen`,
   },
   {
     title: "jsii-publish",
-    description: "Dockerfile and GitHub action for publishing JSII packages. Package building and publishing to npm, PyPI, NuGet and Maven (GitHub)",
+    description:
+      "Dockerfile and GitHub action for publishing JSII packages. Package building and publishing to npm, PyPI, NuGet and Maven (GitHub)",
     href: `https://github.com/udondan/jsii-publish`,
   },
   {
     title: "RocketCDK",
-    description: "Update versions of your packages and CDK version in one command. Works with TS and Python",
+    description:
+      "Update versions of your packages and CDK version in one command. Works with TS and Python",
     href: `https://www.npmjs.com/package/rocketcdk`,
   },
 ]
@@ -59,33 +59,45 @@ function Tools(props: Props): ReactElement {
     <Layout>
       <Nav title="Tools" />
 
-      <section className="flex-grow px-4 flex flex-col w-screen justify-start bg-gray-800 text-gray-100">
-        <div className="p-6">
-          <h1 className="text-6xl mb-2">CDK Dev Tools</h1>
-          <p className="mb-8">
-            This is a collection of tools to help during the development of CDK
-            applications.
-          </p>
-        </div>
-
-        <div className="flex flex-row mb-8">
-          {projects.map(project => (
-            <Project {...project} />
-          ))}
-        </div>
-
-        <div className="p-8 flex flex-col">
-          <h3 className="text-3xl mb-6 text-center">
-            Want to contribute or learn more?
-          </h3>
-
-          <a href="https://github.com/cdk-dev" className="mx-auto mb-4">
-            <div className={`rounded-full bg-gray-700 inline-block px-6 py-3`}>
-              View on GitHub
+      <div className="py-10">
+        <header>
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold leading-tight text-gray-900">
+              CDK Dev Tools
+            </h1>
+          </div>
+        </header>
+        <main>
+          <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 text-gray-900">
+            <div className="px-4 py-8 sm:px-0">
+              <p className="mb-8">
+                This is a collection of tools to help during the development of
+                CDK applications.
+              </p>
             </div>
-          </a>
-        </div>
-      </section>
+
+            <div className="flex flex-row mb-8">
+              {projects.map((project) => (
+                <Project {...project} />
+              ))}
+            </div>
+
+            <div className="p-8 flex flex-col">
+              <h3 className="text-3xl mb-6 text-center">
+                Want to contribute or learn more?
+              </h3>
+
+              <a href="https://github.com/cdk-dev" className="mx-auto mb-4">
+                <div
+                  className={`rounded-full bg-gray-700 inline-block px-6 py-3`}
+                >
+                  View on GitHub
+                </div>
+              </a>
+            </div>
+          </div>
+        </main>
+      </div>
     </Layout>
   )
 }

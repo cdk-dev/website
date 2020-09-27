4dba23a858779fdea34962dc65ebc967f89964a0
Sebastian Korfmann <sebastian@korfmann.net>
2020-09-27 20:50:53 +0200

    CTA to add content

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -76,7 +76,7 @@ export default ({ posts }) => (
     <Hero />
     <Logos />
 
-    <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+    <div className="relative bg-gray-50 pt-16 pb-4 px-4 sm:px-6 lg:pt-24 lg:pb-6 lg:px-8">
       <div className="absolute inset-0">
         <div className="bg-white h-1/3 sm:h-2/3"></div>
       </div>
@@ -93,6 +93,26 @@ export default ({ posts }) => (
           {posts.map((post) => (
             <Post key={post.title} post={post} />
           ))}
+        </div>
+      </div>
+    </div>
+
+    <div className="bg-gray-50">
+      <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+        <h2 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+          Created or seen other CDK related content?
+          <br />
+          <span className="text-indigo-600">Share it with the community</span>
+        </h2>
+        <div className="mt-8 flex lg:flex-shrink-0 lg:mt-0">
+          <div className="inline-flex rounded-md shadow">
+            <a
+              href="https://github.com/cdk-dev/website/issues/new?assignees=&labels=content&template=content.md&title="
+              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+            >
+              Create Pull Request
+            </a>
+          </div>
         </div>
       </div>
     </div>

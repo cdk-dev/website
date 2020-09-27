8ed9ae3668e242185d5eb4eb353fba7435b7a29c
Sebastian Korfmann <sebastian@korfmann.net>
2020-09-27 21:27:13 +0200

    Fix hero menu

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -96,6 +96,30 @@ function Nav({ title }: NavProps): ReactElement {
                   <a href="#" aria-label="Home">
                     cdk.dev
                   </a>
+                  <div className="-mr-2 flex items-center md:hidden">
+                    <button
+                      type="button"
+                      className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                      id="main-menu"
+                      aria-label="Main menu"
+                      aria-haspopup="true"
+                      onClick={() => setIsOpen(!isOpen)}
+                    >
+                      <svg
+                        className="h-6 w-6"
+                        stroke="currentColor"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M4 6h16M4 12h16M4 18h16"
+                        />
+                      </svg>
+                    </button>
+                  </div>
                 </div>
               </div>
               <div className="hidden md:flex md:space-x-10">
@@ -127,50 +151,53 @@ function Nav({ title }: NavProps): ReactElement {
             </nav>
           </div>
 
-          <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-            <div className="rounded-lg shadow-md">
+          <Transition
+            show={isOpen}
+            enter="transition ease-out duration-100 transform"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="transition ease-in duration-75 transform"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            {(ref) => (
               <div
-                className="rounded-lg bg-white shadow-xs overflow-hidden"
-                role="menu"
-                aria-orientation="vertical"
-                aria-labelledby="main-menu"
+                ref={ref}
+                className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
               >
-                <div className="px-5 pt-4 flex items-center justify-between">
-                  <div>cdk.dev</div>
-                  <div className="-mr-2">
-                    <button
-                      type="button"
-                      onClick={() => setIsOpen(!isOpen)}
-                      className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-                      aria-label="Close menu"
-                    >
-                      <svg
-                        className="h-6 w-6"
-                        stroke="currentColor"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M6 18L18 6M6 6l12 12"
-                        />
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-                <Transition
-                  show={isOpen}
-                  enter="transition ease-out duration-100 transform"
-                  enterFrom="opacity-0 scale-95"
-                  enterTo="opacity-100 scale-100"
-                  leave="transition ease-in duration-75 transform"
-                  leaveFrom="opacity-100 scale-100"
-                  leaveTo="opacity-0 scale-95"
-                >
-                  {(ref) => (
-                    <div ref={ref} className="px-2 pt-2 pb-3">
+                <div className="rounded-lg shadow-md">
+                  <div
+                    className="rounded-lg bg-white shadow-xs overflow-hidden"
+                    role="menu"
+                    aria-orientation="vertical"
+                    aria-labelledby="main-menu"
+                  >
+                    <div className="px-5 pt-4 flex items-center justify-between">
+                      <div>cdk.dev</div>
+                      <div className="-mr-2">
+                        <button
+                          type="button"
+                          onClick={() => setIsOpen(!isOpen)}
+                          className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                          aria-label="Close menu"
+                        >
+                          <svg
+                            className="h-6 w-6"
+                            stroke="currentColor"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M6 18L18 6M6 6l12 12"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+                    <div className="px-2 pt-2 pb-3">
                       <a
                         href="/tools"
                         className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
@@ -200,11 +227,11 @@ function Nav({ title }: NavProps): ReactElement {
                         CDK Day
                       </a>
                     </div>
-                  )}
-                </Transition>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            )}
+          </Transition>
           <main className="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
             <div className="text-center">
               <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">

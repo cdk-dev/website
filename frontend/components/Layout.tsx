5df3d8be9133ffa798c03d963fdc6d23420c9dd0
Sebastian Korfmann <sebastian@korfmann.net>
2020-09-28 13:22:11 +0200

    Add favicon

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -8,6 +8,27 @@ export default ({ children }): ReactElement => (
       <title>cdk.dev</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
     <div className="min-h-screen bg-white">
       {children}

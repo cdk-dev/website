399d2d0e9abda25b71f01ba589044eedc8634eb0
Sebastian Korfmann <sebastian@korfmann.net>
2020-09-28 20:28:35 +0200

    Link post images as well

--- frontend/components/Post.tsx
+++ frontend/components/Post.tsx
@@ -12,7 +12,13 @@ function Post({ post }): ReactElement {
   return (
     <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
       <div className="flex-shrink-0">
-        <img className="h-48 w-full object-cover" src={post.image} alt="foo" />
+        <a href={post.url}>
+          <img
+            className="h-48 w-full object-cover"
+            src={post.image}
+            alt="foo"
+          />
+        </a>
       </div>
       <div className="flex-1 bg-white p-6 flex flex-col justify-between">
         <div className="flex-1">

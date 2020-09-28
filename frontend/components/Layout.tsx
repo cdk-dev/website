1d3f26277d75f054cfb1b7414b9219b0f13ab53f
Sebastian Korfmann <sebastian@korfmann.net>
2020-09-28 15:49:01 +0200

    Add open graph meta data and image

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from "react"
 import Footer from "./Footer"
 import Head from "next/head"
 
-export default ({ children }): ReactElement => (
+export default ({ children, url }): ReactElement => (
   <>
     <Head>
       <title>cdk.dev</title>
@@ -29,6 +29,15 @@ export default ({ children }): ReactElement => (
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="theme-color" content="#ffffff" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@cdkdev_" />
+      <meta name="og:title" content="cdk.dev" />
+      <meta
+        name="og:description"
+        content="The community driven hub around the Cloud Development Kit (CDK) ecosystem. This site brings together all the latest blogs, videos, and educational content. Connect with the community of AWS CDK, CDK for Kubernetes (cdk8s) and CDK for Terraform (cdktf)."
+      />
+      <meta name="og:image" content={`${url}/og.png`}></meta>
     </Head>
     <div className="min-h-screen bg-white">
       {children}
@@ -36,3 +45,9 @@ export default ({ children }): ReactElement => (
     </div>
   </>
 )
+
+export async function getStaticProps() {
+  return {
+    props: { url: process.env.DEPLOY_PRIME_URL || "http://localhost:3000" },
+  }
+}

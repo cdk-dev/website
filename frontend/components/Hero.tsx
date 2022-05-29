bcd33d2081adeb313a0fdfef67bac98c28052eb9
Matthew Bonig <matthew.bonig@gmail.com>
2022-05-29 16:16:46 -0600

    updating link

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -265,7 +265,7 @@ function Nav({ title }: NavProps): ReactElement {
               <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
                 <div className="rounded-md shadow">
                   <a
-                    href="https://join.slack.com/t/cdk-dev/shared_invite/zt-19aj6e67n-r0DeQKRhnR7AiogvAFMaMA"
+                    href="https://join.slack.com/t/cdk-dev/shared_invite/zt-19nwray07-px8F1A_Ubf5~TsKHUgbL1w"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
                   >
                     Join Slack

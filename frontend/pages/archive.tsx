54d9e0e27f84264cc6e3a84419caccd5b412b119
Martin Mueller <damadden88@googlemail.com>
2023-02-21 18:08:47 +0000

    feature: create archive page

---
+++ frontend/pages/archive.tsx
@@ -0,0 +1,96 @@
+import React, { ReactElement } from "react"
+
+import Nav from "../components/Nav"
+import Layout from "../components/Layout"
+import CreateContent from "../components/CreateContent"
+import Newsletter from "../components/Newsletter"
+
+function Archive({
+  archiveMessages,
+}: {
+  archiveMessages: Message[]
+}): ReactElement {
+  return (
+    <Layout>
+      <Nav title="Archive" />
+
+      <div className="py-10">
+        <header>
+          <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold leading-tight text-gray-900">
+              Posts
+            </h1>
+          </div>
+        </header>
+        <main>
+          <div className="mx-auto text-gray-900 max-w-7xl sm:px-6 lg:px-8">
+            <div className="px-4 py-6 sm:px-0">
+              <p className="mb-8">Archived messages.</p>
+            </div>
+
+            <div className="grid max-w-lg gap-5 mx-auto mt-0 lg:grid-cols-3 lg:max-w-none">
+              {archiveMessages.map((archivedMessage) => {
+                return JSON.stringify(archivedMessage)
+              })}
+            </div>
+          </div>
+        </main>
+      </div>
+
+      <CreateContent />
+      <Newsletter />
+    </Layout>
+  )
+}
+
+export default Archive
+
+export async function getStaticProps() {
+
+  // fetch messages and threads from database
+  const archiveMessages: Message[] = [
+    {
+      channelId: "1",
+      channelName: "aws-cdk",
+      author: "Matthew",
+      timestamp: 11122,
+      text: "hello world",
+    },
+    {
+      channelId: "2",
+      channelName: "jobs",
+      author: "Martin",
+      timestamp: 11332,
+      text: "need a job!",
+      thread: [
+        {
+          author: "Matthew",
+          timestamp: 11333,
+          text: "I have carpenter position available",
+        },
+        {
+          author: "John",
+          timestamp: 11334,
+          text: "McDonalds is looking as well",
+        },
+      ],
+    },
+  ]
+
+  return { props: { archiveMessages } }
+}
+
+interface Message {
+  channelId: string
+  channelName: string
+  author: string
+  timestamp: number
+  text: string
+  thread?: ThreadMessage[]
+}
+
+interface ThreadMessage {
+  author: string
+  text: string
+  timestamp: number
+}

dcefd1f5c1dd650702137310925cbf7640ddb2e7
Matthew Bonig <matthew.bonig@gmail.com>
2023-11-03 08:24:30 -0600

    updating invite link

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -265,7 +265,7 @@ function Nav({ title }: NavProps): ReactElement {
               <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
                 <div className="rounded-md shadow">
                   <a
-                    href="https://join.slack.com/t/cdk-dev/shared_invite/zt-1vcqzk7kn-W73TrmhKVuIEWP2r8SelsA"
+                    href="https://join.slack.com/t/cdk-dev/shared_invite/zt-26jb5mbg1-3w9nmpvj2HAK6NmvuQAPCg"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
                   >
                     Join Slack

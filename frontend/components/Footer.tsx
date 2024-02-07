9c9c63f1f82099be6dd5334ea5ef72998cef2332
Matthew Bonig <matthew.bonig@gmail.com>
2024-02-07 07:06:11 -0700

    updating slack invite

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -57,7 +57,7 @@ function Footer(): ReactElement {
         </nav>
         <div className="mt-8 flex justify-center">
           <a
-            href="https://join.slack.com/t/cdk-dev/shared_invite/zt-26jb5mbg1-3w9nmpvj2HAK6NmvuQAPCg"
+            href="https://join.slack.com/t/cdk-dev/shared_invite/zt-2c9cac9sk-L3K0nGwxmZDDqhQ9iM6VFw"
             className="text-gray-400 hover:text-gray-500"
           >
             <span className="sr-only">Slack</span>

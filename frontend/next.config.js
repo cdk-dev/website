492af308654ddf6032a0b22044a16f0f2ed5fc54
Sebastian Korfmann <sebastian@korfmann.net>
2024-04-25 21:59:36 +0200

    Don't autoload yml from nextjs context
    this was causing all sorts of build issues

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,15 @@
-const withOptimizedImages = require("next-optimized-images")
-module.exports = Object.assign(withOptimizedImages(), {
+const withOptimizedImages = require("next-optimized-images");
+
+module.exports = withOptimizedImages({
   publicRuntimeConfig: {
     deployUrl: process.env.DEPLOY_PRIME_URL || "http://localhost:3000",
   },
-})
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.ya?ml$/,
+      loader: 'ignore-loader'  // Requires installing ignore-loader
+    });
+
+    return config;
+  }
+});

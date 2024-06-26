0cabc1f1590c3b3ea43306b3f9897c1e24733fb8
Sebastian Korfmann <sebastian@korfmann.net>
2024-06-26 22:21:12 +0200

    add plausible analytics

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+import { withPlausibleProxy } from 'next-plausible'
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -12,4 +14,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPlausibleProxy()(nextConfig);
